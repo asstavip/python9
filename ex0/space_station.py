582507dbff9d4e56fd8bd647492bc0613f3180f6
abderrahman <abdo.bissari@gmail.com>
2026-02-08 18:33:39 +0100

    done

--- ex0/space_station.py
+++ ex0/space_station.py
@@ -1,35 +1,50 @@
-from pydantic import BaseModel, Field
-from typing import Annotated,Literal
-# station_id: String, 3-10 characters
-# • name: String, 1-50 characters
-# • crew_size: Integer, 1-20 people
-# • power_level: Float, 0.0-100.0 percent
-# • oxygen_level: Float, 0.0-100.0 percent
-# • last_maintenance: DateTime field
-# • is_operational: Boolean, defaults to True
-# • notes: Optional string, max 200 characters
+from pydantic import BaseModel, Field, ValidationError
+from typing import Optional
+from datetime import datetime
 
 class SpaceStation(BaseModel):
-    pass
+    station_id: str = Field(min_length=3, max_length=10)
+    name: str = Field(min_length=1, max_length=50)
+    crew_size: int = Field(ge=1, le=20)
+    power_level: float = Field(ge=0, le=100)
+    oxygen_level: float = Field(ge=0, le=100)
+    last_maintenance: datetime
+    is_operational: bool = True
+    notes: Optional[str] = Field(default=None,max_length=200)
 
 
-
-# Space Station Data Validation
-# ========================================
-# Valid station created:
-# ID: ISS001
-# Name: International Space Station
-# Crew: 6 people
-# Power: 85.5%
-# Oxygen: 92.3%
-# Status: Operational
-# ========================================
-# Expected validation error:
-# Input should be less than or equal to 20
-def main():
-    pass
+s = SpaceStation(
+    station_id="ISS001",
+    name="International Space Station",
+    crew_size=6,
+    power_level=85.5,
+    oxygen_level=92.3,
+    is_operational=True,
+    last_maintenance=datetime.now(),
+)
 
 
+print(f"""
+Space Station Data Validation
+========================================
+Valid station created:
+ID: {s.station_id}
+Name: {s.name}
+Crew: {s.crew_size} people
+Power: {s.power_level}%
+Oxygen: {s.oxygen_level}%
+Status: {"Operational" if s.is_operational else "Not Operational"}
+========================================""")
 
-if __name__ == "__main__":
-    main()
+try:
+    s = SpaceStation(
+    station_id="ISS002",
+    name="International Space Station",
+    crew_size=30,
+    power_level=15.2,
+    oxygen_level=92.3,
+    is_operational=True,
+    last_maintenance=datetime.now(),
+    )
+except ValidationError as e:
+    print(e)
---
+++ ex1/alien_contact.py
@@ -0,0 +1,77 @@
+from pydantic import BaseModel, Field, ValidationError, model_validator
+from typing import Optional
+from datetime import datetime
+from enum import Enum
+
+
+class ContactType(Enum):
+    RADIO = "RADIO"
+    VISUAL = "VISUAL"
+    PHYSICAL = "PHYSICAL"
+    TELEPATHIC = "TELEPATHIC"
+
+
+class AlienContact(BaseModel):
+    contact_id: str = Field(min_length=5,max_length=15)
+    timestamp : datetime
+    location: str = Field(min_length=3,max_length=100)
+    contact_type:ContactType
+    signal_strength:float = Field(ge=0,le=10)
+    duration_minutes:int = Field(ge=1,le=1440)
+    message_received: Optional[str] = Field(default=None,max_length=500)
+    witness_count:int = Field(ge=1,le=100)
+    is_verified: bool = False
+
+    @model_validator(mode='after')
+    def validate(self):
+        if not self.contact_id.startswith("AC"):
+            raise ValueError("Contact ID must start with 'AC'")
+        if not self.is_verified:
+            raise ValueError("Physical contact reports must be verified")
+        if self.contact_type == ContactType.TELEPATHIC and self.witness_count < 3:
+            raise ValueError("Telepathic contact requires at least 3 witnesses")
+        if  self.signal_strength > 7 and self.message_received == None:
+            raise ValueError("Strong signals (> 7.0) should include received messages")
+        return self
+    
+
+valid_contact = AlienContact(
+contact_id="AC_2024_001",
+timestamp=datetime(2024, 1, 15, 22, 30, 0),
+location="Area 51, Nevada",
+contact_type=ContactType.RADIO,
+signal_strength=8.5,
+duration_minutes=45,
+witness_count=5,
+message_received="Greetings from Zeta Reticuli",
+is_verified = True
+)
+print(f"""
+Alien Contact Log Validation
+======================================
+Valid contact report:
+ID: {valid_contact.contact_id}
+Type: {valid_contact.contact_type}
+Location: {valid_contact.location}
+Signal: {valid_contact.signal_strength}/10
+Duration: {valid_contact.duration_minutes} minutes
+Witnesses: {valid_contact.witness_count}
+Message: {valid_contact.message_received}""")
+try:
+
+    print("======================================")
+
+    not_valid = AlienContact(
+        contact_id="AC_2024_001",
+        timestamp=datetime(2024, 1, 15, 22, 30, 0),
+        location="Area 51, Nevada",
+        contact_type=ContactType.TELEPATHIC,
+        signal_strength=8.5,
+        duration_minutes=45,
+        witness_count=2,
+        message_received="Greetings from Zeta Reticuli",
+        is_verified=True,
+    )
+except ValidationError as e:
+    print("Validation error:", e)
+
